5c3fc34141a6336f30abff2f753f2086b97a0ccb
adiyz <adiyzofrul@gmail.com>
2026-07-23 16:04:21 +0800

    git test staging

--- src/p02helloworld02/HelloWorld02.java
+++ src/p02helloworld02/HelloWorld02.java
@@ -2,6 +2,6 @@ package p02helloworld02;
 
 public class HelloWorld02 {
     public static void main(String[] args) {
-        System.out.println("Hello World 02");
+        System.out.println("Hello World 02. test Staging");
     }
 }
